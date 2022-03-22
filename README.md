106f2c2a966598961fb99f4f00720e8423a2d757
Simon JACQUEMIN <jacqueminsimon@gmail.com>
2022-03-22 08:55:02 +0100

    clear node modules

---
+++ README.md
@@ -0,0 +1,4 @@
+# remote-test-frontend
+
+- `yarn` to install the application.
+- `yarn start` to run the application.
---
+++ src/types/game.ts
@@ -0,0 +1,15 @@
+
+  export type Country = {
+    name: string,
+    capital: string,
+  }
+  
+  export type AnswerCounter = {
+    badAnswers: number,
+    goodAnswers: number,
+  }
+
+  export enum InfoText {
+    WIN = 'Gagné !',
+    LOOSE = 'Perdu'
+}
